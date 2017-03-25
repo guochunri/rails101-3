bea161797ecd95b7486780464fa764b7c4fccffb
guochunri <guochunri@gmail.com>
2017-03-25 19:22:14 +0800

    add edit & delete action in post

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -2,9 +2,16 @@ class PostsController < ApplicationController
 
   before_action :authenticate_user!, :only => [:new, :create]
 
+
+
   def new
     @group = Group.find(params[:group_id])
     @post = Post.new
+  end
+
+  def edit
+    @group = Group.find(params[:group_id])
+    @post = Post.find(params[:id])
   end
 
   def create
@@ -18,6 +25,23 @@ class PostsController < ApplicationController
     else
       render :new
     end
+  end
+
+  def update
+    @group = Group.find(params[:group_id])
+    @post = Post.find(params[:id])
+    if @post.update(post_params)
+      redirect_to account_posts_path(@group), notice: "Update Success"
+    else
+      render :edit
+    end
+  end
+
+  def destroy
+      @group = Group.find(params[:group_id])
+      @post = Post.find(params[:id])
+      @post.destroy
+      redirect_to account_posts_path(@group), alert: "Post deleted"
   end
 
 
